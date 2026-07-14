ccebcdef88f77484b1bfa7cdc6d4de30e44693c8
vineetsharm55-hub <vineetsharm55@gmail.com>
2026-07-14 18:59:16 +0530

    Time: 3 ms (32.67%), Space: 14.9 MB (16.69%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        int n=nums1.size();
+        int n1=nums2.size();
+        map<int,int>mp;
+        vector<int>ans;
+
+        for(int i=0;i<n;i++){
+            mp[nums1[i]]++;
+        }
+        for(int i=0;i<n1;i++){
+            if(mp[nums2[i]]>0){
+                ans.push_back(nums2[i]);
+            }
+            mp[nums2[i]]--;
+        }
+        return ans;
+    }
+};
