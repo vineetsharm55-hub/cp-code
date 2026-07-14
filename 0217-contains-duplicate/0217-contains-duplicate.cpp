582e835bdfe68452b7d0e299f0e6f80b0c957688
vineetsharm55-hub <vineetsharm55@gmail.com>
2026-07-14 17:02:08 +0530

    Time: 76 ms (19.72%), Space: 111.4 MB (13.89%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,11 +1,25 @@
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//        sort(nums.begin(),nums.end());
+//        for(int i=1;i<nums.size();i++){
+//             if(nums[i]==nums[i-1]){
+//                 return true;
+//             }
+//        }
+//        return false;
+//     }
+// };
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-       sort(nums.begin(),nums.end());
-       for(int i=1;i<nums.size();i++){
-            if(nums[i]==nums[i-1]){
-                return true;
-            }
+       unordered_map<int,int>sp;
+       int n=nums.size();
+       for(int i=0;i<n;i++){
+        if(sp.find(nums[i])!=sp.end()){
+            return true;
+        }
+        sp[nums[i]]=i;
        }
        return false;
     }
