257cbb9ddcdc31d93a308ab284c28f620ff07b84
vineetsharm55-hub <vineetsharm55@gmail.com>
2026-07-17 08:24:07 +0530

    Time: 0 ms (100%), Space: 21.8 MB (71.02%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,16 +1,29 @@
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         int n=nums.size();
+//         int a=0;
+//         int b=0;
+//         for(int i=0;i<n;i++){
+//             a=a+nums[i];
+//         }
+//         for(int i=0;i<=n;i++){
+//             b=b+i;
+//         }
+//         int c=b-a;
+//         return c;
+//     }
+// };
+
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n=nums.size();
-        int a=0;
-        int b=0;
+        int XOR=nums.size();
         for(int i=0;i<n;i++){
-            a=a+nums[i];
+            XOR ^=i;
+            XOR ^=nums[i];
         }
-        for(int i=0;i<=n;i++){
-            b=b+i;
-        }
-        int c=b-a;
-        return c;
+        return XOR;
     }
 };
