84aa23f49bec164156170aecc0daae948480545d
vineetsharm55-hub <vineetsharm55@gmail.com>
2026-08-06 03:20:36 +0530

    Time: 0 ms (100%), Space: 16 MB (61.52%) - LeetHub

--- 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.cpp
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.cpp
@@ -4,12 +4,11 @@ public:
         int totalSum = 0;
         int leftSum = 0;
 
-        // Calculate total sum
+    
         for (int num : nums) {
             totalSum += num;
         }
 
-        // Find middle index
         for (int i = 0; i < nums.size(); i++) {
             int rightSum = totalSum - leftSum - nums[i];
 
